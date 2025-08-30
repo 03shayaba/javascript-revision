f89bcca591006ccb8e0acd442b7b3c1f5a153274
03shayaba <03shayaba@users.noreply.github.com>
2025-08-30 18:53:01 +0530

    form-valiadtion and  some new changes in my project

--- project/script.js
+++ project/script.js
@@ -1,14 +1,42 @@
 const rows = document.querySelectorAll(".row1");
-  rows.forEach((box) => {
-    box.addEventListener("click", () => {
-      // Reset sabka color
-      rows.forEach((el) => {
-        el.style.backgroundColor = "";
-        el.style.color = "";
-      });
-
-      // Sirf clicked element ka color change karo
-      box.style.backgroundColor = "rgb(247,245,255)";
-      box.style.color = "black";
+rows.forEach((box) => {
+  box.addEventListener("click", () => {
+    // Reset sabka color
+    rows.forEach((el) => {
+      el.style.backgroundColor = "";
+      el.style.color = "";
     });
+
+    // Sirf clicked element ka color change karo
+    box.style.backgroundColor = "rgb(247,245,255)";
+    box.style.color = "black";
   });
+});
+
+let row1 = document.querySelector("#first-row");
+let img0 = document.getElementById("change-image");
+
+row1.addEventListener("click", function (dets) {
+  dets.preventDefault();
+  img.setAttribute("src", "org.avif");
+});
+
+let row2 = document.querySelector("#sec-row");
+let img = document.getElementById("change-image");
+
+row2.addEventListener("click", function (dets) {
+  dets.preventDefault();
+  img.setAttribute("src", "change1.jpg");
+});
+
+let row3 = document.querySelector("#third-row");
+let img1 = document.getElementById("change-image");
+
+row3.addEventListener("click", function (dets) {
+  dets.preventDefault();
+  img1.setAttribute("src", "change2.avif");
+});
+
+
+
+
