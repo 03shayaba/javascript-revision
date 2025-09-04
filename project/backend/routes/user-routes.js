dc658f9fd35074bcc98bbda49b8fa07347e284dd
03shayaba <03shayaba@users.noreply.github.com>
2025-09-04 22:48:35 +0530

    a

--- project/backend/routes/user-routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { userdetails, userLogin } from '../controller/user.js';
-
- const router = express.Router();
-
-router.get('/login' , userLogin);
-router.get("/signup" , userdetails);
-export default router;
